dfe1b1a9cd79481c5385b47bda3bac456e5f2637
gustmg <gustavo.mitre.gallardo@gmail.com>
2019-03-18 18:16:31 -0600

    Feat: Client form added

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const mdc = require('material-components-web');
+mdc.autoInit();
+
 import {MDCTextField} from '@material/textfield';
 
 const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
