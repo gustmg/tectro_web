b12dc2d3a1181098335fff349b27f931c0cb99e4
gustmg <gustavo.mitre.gallardo@gmail.com>
2019-03-20 14:48:57 -0600

    Feat: Mobile client form
    - Added mobile client form
    - Updated dekstop footer structure

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const mdc = require('material-components-web');
 mdc.autoInit();
 
-import {MDCTextField} from '@material/textfield';
-
-const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+import {MDCDialog} from '@material/dialog';
+const dialog = new MDCDialog(document.querySelector('.mdc-dialog'));
